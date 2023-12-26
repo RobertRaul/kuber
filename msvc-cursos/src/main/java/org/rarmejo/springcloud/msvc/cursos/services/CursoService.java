da721be5a8d9d5716a833192df0777bd22f44e4b
Robert Raul <lordnebr4@gmail.com>
2023-12-26 09:21:17 -0500

    Inicio commit 2

---
+++ msvc-cursos/src/main/java/org/rarmejo/springcloud/msvc/cursos/services/CursoService.java
@@ -0,0 +1,21 @@
+package org.rarmejo.springcloud.msvc.cursos.services;
+
+import org.rarmejo.springcloud.msvc.cursos.models.Usuario;
+import org.rarmejo.springcloud.msvc.cursos.models.entity.Curso;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CursoService {
+    List<Curso> listar();
+    Optional<Curso> porId(Long id);
+    Optional<Curso> porIdConUsuarios(Long id);
+    Curso guardar(Curso curso);
+    void eliminar(Long id);
+    void eliminarCursoUsuarioPorId(Long id);
+
+    //servicios para comunicarnos con el sevicio Usuario
+    Optional<Usuario> asignarUsuario(Usuario usuario, Long cursoId);
+    Optional<Usuario> crearUsuario(Usuario usuario, Long cursoId);
+    Optional<Usuario> eliminarUsuario(Usuario usuario, Long cursoId);
+}
---
+++ msvc-usuarios/src/main/java/org/rarmejo/springcloud/msvc/usuarios/services/UsuarioService.java
@@ -0,0 +1,19 @@
+package org.rarmejo.springcloud.msvc.usuarios.services;
+
+import org.rarmejo.springcloud.msvc.usuarios.models.entity.Usuario;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+    List<Usuario> listar();
+    Optional<Usuario> porId(Long id);
+    Usuario guardar(Usuario usuario);
+    void eliminar(Long id);
+
+    List<Usuario> listarPorIds(Iterable<Long> ids);
+
+    Optional<Usuario> porEmail(String email);
+    boolean existePorEmail(String email);
+}
